c6124be7b1340d49fa64adfa9d80bd2cb0df97a3
Chetan Pandey <chetan@spheron.network>
2024-08-20 21:55:49 +0530

    script fix

--- charts/gw/scripts/run.sh
+++ charts/gw/scripts/run.sh
@@ -13,7 +13,6 @@ type bc || exit 1
 echo "wallet: $SPHERON_FROM"
 echo "home: $SPHERON_HOME"
 echo "secret: $SPHERON_KEY_SECRET" ## TODO: Remove this not good for data compliance
-env
 
 exec provider-services run \
     --from=$SPHERON_FROM \
