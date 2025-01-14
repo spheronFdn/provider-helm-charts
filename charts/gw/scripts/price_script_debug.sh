b2bac7df7dfc588649091886dd5cf401af74b381
Sumit Agrawal <mr.sumitagrawal.17@gmail.com>
2025-01-14 18:17:39 +0530

    Removed gateway helm chart from helm charts

--- charts/gw/scripts/price_script_debug.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-# for debugging the bid price engine
-
-echo "====================== start ======================" >> /tmp/log
-cat - >> /tmp/log
-
-echo "====================== end ======================" >> /tmp/log
-echo 99999
